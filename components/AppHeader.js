33cecebcb7308eee559ccf4fcb41ab018d8c07b7
mn_h02 <mn_h02@example.local>
2026-07-30 20:59:39 +0900

    Move Beyond nav to right side

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -8,13 +8,13 @@ export default function AppHeader() {
         <Link href="/search">검색</Link>
         <Link href="/reviews">피드</Link>
         <Link href="/news">뉴스</Link>
-        <Link href="/beyond-your-fence">Beyond</Link>
         <Link href="/about">소개</Link>
       </nav>
 
       <a className="logo" href="/" aria-label="청음록 메인으로 이동">청음록</a>
 
       <nav className="headerNav headerNavRight" aria-label="사용자 메뉴">
+        <Link href="/beyond-your-fence">Beyond</Link>
         <Link href="/profile">내 기록</Link>
         <Link href="/write">기록하기</Link>
         <AuthNav />
