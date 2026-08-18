b0b0ad7198a4e96dacd362c48adb4ffff7553f03
mn_h02 <mn_h02@example.local>
2026-08-18 22:27:30 +0900

    Revert "Apply darker home landing design"
    This reverts commit 927aa017ac912bfe196d145223ffd3eb3ac9583c.

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -7,7 +7,8 @@ export default function AppHeader() {
       <nav className="headerNav headerNavLeft" aria-label="주요 메뉴">
         <Link href="/search">Search</Link>
         <Link href="/reviews">Feed</Link>
-        <Link href="/archive">Archive</Link>
+        <Link href="/news">News</Link>
+        <Link href="/about">About</Link>
       </nav>
 
       <Link className="logo" href="/" aria-label="청음록 메인으로 이동">청음록</Link>
@@ -15,6 +16,7 @@ export default function AppHeader() {
       <nav className="headerNav headerNavRight" aria-label="사용자 메뉴">
         <Link href="/beyond-your-fence">Beyond</Link>
         <Link href="/profile">My Records</Link>
+        <Link href="/write">Write</Link>
         <AuthNav />
       </nav>
     </header>
